86cbd3d5aa1895930103a7bc00a24f453e237fbb
simlexx-k <simonwriter254@gmail.com>
2024-05-10 17:07:55 -0400

    install flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './css/styles.css'
 import Hero from './components/hero'
 import Features from './components/features'
+import Pricing from './components/pricing'
+
 function App() {
   return (
     <div className="App">
       <Hero/>
       <Features/>
+        <Pricing/>
     </div>
 
   );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -14,7 +14,7 @@ export default function Example() {
 
   return (
     <div className="bg-white mt-0">
-      <header className="fixed bg-white dark:bg-black inset-x-0 top-0 z-50">
+      <header className="fixed backdrop-blur-xl bg-white/30 dark:bg-black inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">

c70053c0c7acca84efcd8acab52026bfccbf65ae
simlexx-k <simonwriter254@gmail.com>
2024-05-10 16:44:42 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './css/styles.css'
+import Hero from './components/hero'
+import Features from './components/features'
+function App() {
+  return (
+    <div className="App">
+      <Hero/>
+      <Features/>
+    </div>
+
+  );
+  // return ();
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
